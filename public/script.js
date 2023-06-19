5262321864f0d20c067379e78d16bca5e56f3367
Int Fract <intfract@gmail.com>
2023-06-19 07:48:13 +0000

    reduced repetition

--- public/script.js
+++ public/script.js
@@ -1,31 +1,3 @@
-function sentence(text) {
-  return text[0].toUpperCase() + text.substr(1)
-}
-
-function camel(text) {
-  const words = text.split('-')
-  for (let i = 0; i < words.length; i++) {
-    if (i) words[i] = sentence(words[i])
-  }
-  return words.join('')
-}
-
-function pascal(text) {
-  const words = text.split('-')
-  for (let i = 0; i < words.length; i++) {
-    words[i] = sentence(words[i])
-  }
-  return words.join('')
-}
-
-function url(text, context) {
-  return context + '/' + text.toLowerCase().split(' ').join('_')
-}
-
-function component(name, element) {
-  return mdc[camel(name)][`MDC${pascal(name)}`].attachTo(element)
-}
-
 function navigate(pathname, current) {
   transition()
   for (const item of nav.children) {
